612129e81938f84ed91d0d7e3599934e1c8a9eba
JamaJ1701 <48624318+JamaJ1701@users.noreply.github.com>
2023-01-08 10:06:29 +1000

    Alt tag added to image

--- src/components/SectionSubComponents.tsx
+++ src/components/SectionSubComponents.tsx
@@ -8,7 +8,7 @@ import { Typography, Box } from "@mui/material";
 export function ImageWithCaption(prop: { image: string, caption: string, class:string }) {
     return (
         <Box textAlign='center'>
-            <img className={prop.class} src={prop.image}/>
+            <img className={prop.class} src={prop.image} alt={prop.caption}/>
             <Typography m={0} sx={{color:"#6a6a6a"}}>
                 {prop.caption}
             </Typography>
